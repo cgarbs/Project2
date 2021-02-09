a45c570f8acfe8fd1c804f2a1bd21e045fb4b72c
Christian <christian.garba@gmail.com>
2021-02-08 18:48:45 -0700

    successfully deployed

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,29 +1,28 @@
-// Iteration #1
+const mongoose = require("mongoose");
 
-const mongoose = require('mongoose');
+require("../configs/db.config");
 
-require('../configs/db.config');
-
-const Job = require('../models/Job.model');
+const Job = require("../models/Job.model");
 
 const jobs = [
-    { name: 'Wedding Photographer' },
-    { name: 'Wedding Videographer' },
-    { name: 'Commerical Photographer' },
-    { name: 'Commerical Videographer' },
-    { name: 'Portrait Photographer' },
-    { name: 'Lifestyle Photographer' },
-    { name: 'Lifestyle Videographer' },
-    { name: 'Sports Photographer' },
-    { name: 'Sports Cameraman' },
-    { name: 'Animal Photographer' },
-    { name: 'Product Photographer' },
-    { name: 'Photojournalist' }
+  { name: "Wedding Photographer" },
+  { name: "Wedding Videographer" },
+  { name: "Commerical Photographer" },
+  { name: "Commerical Videographer" },
+  { name: "Portrait Photographer" },
+  { name: "Lifestyle Photographer" },
+  { name: "Lifestyle Videographer" },
+  { name: "Sports Photographer" },
+  { name: "Sports Cameraman" },
+  { name: "Animal Photographer" },
+  { name: "Product Photographer" },
+  { name: "Photojournalist" },
 ];
 
 Job.create(jobs)
-.then(jobsFromDB => {
+  .then((jobsFromDB) => {
     console.log(`Successfully added ${jobsFromDB.length} to the database.`);
+    console.log({ jobsFromDB });
     mongoose.connection.close();
-})
-.catch(err => console.log(`Error occurred - seeding unsuccessful: ${err}`));
+  })
+  .catch((err) => console.log(`Error occurred - seeding unsuccessful: ${err}`));
